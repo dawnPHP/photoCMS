7dc62c548b3f3e5bac5cb525e6d3087f0eaf44e9
Dawn <JimmyMall@live.com>
2015-10-14 22:20:45 +0800

    v0.8.4:for ff preview compitablility add FileReader. IE is not solved yet

--- doImgPreviewUpload.php
+++ doImgPreviewUpload.php
@@ -2,10 +2,7 @@
 //from http://www.cnblogs.com/Zjmainstay/archive/2012/08/09/jQuery_upload_image.html
     //header('content-type:text/html charset:utf-8');  /* 这句要删除，否则可能会导致IE下回传HTML变成下载 */
 	$index=$_POST['index'];
-	include('class/Dir.class.php');
-	include('class/UploadFile.class.php');
-	include('class/MyDebug.class.php');
-	
+
 	//从session获取用户id号，当用户退出时删除该临时文件夹。
 	$userId='5';
 	$dir='images/usrTemp/'.$userId.'/';
@@ -14,14 +11,12 @@
 	if(!file_exists($dir)) mkdir($dir,'0777');
 	
 	//重写file数组
-	$files=UploadFile::rearrange_files_array($_FILES);
-	$files=$files['pic'];	
-	$file=$files[0];
+	$file=$_FILES['pic'];
 	
 	//替换文件名中的空格为下划线
-	$afterReplace = str_replace(' ', '_', $file['name']);
+	$afterReplace = str_replace(' ', '_', $file['name'][0]);
 	$dst = $dir . iconv('utf-8','gbk',$afterReplace);//为文件系统转码
-	move_uploaded_file($file['tmp_name'], $dst );
+	move_uploaded_file($file['tmp_name'][0], $dst );
 
 	//为前台显示拼接图片url
 	$dst =$dir . $afterReplace;
